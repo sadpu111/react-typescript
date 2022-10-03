129e603fbbf09a1c42b38ebef8b453c51505d085
sadpu111 <dwme7100@gmail.com>
2022-10-03 23:10:10 +0900

    optional props

--- src/Circle.tsx
+++ src/Circle.tsx
@@ -2,10 +2,13 @@ import styled from "styled-components";
 
 interface CircleProps { // (1) props로 구성된 객체의 shape을 정의하여(prop type과 유사하지만 코드 실행 전에 에러를 표시한다는 점에서 차이 존재)
   bgColor: string;
+  borderColor?: string; // ?를 추가하여 optional props로 만든다(있어도 되고, 없어도 되는...).
+  text?: string;
 }
 
 interface ContainerProps {
   bgColor: string;
+  borderColor: string;
 }
 
 const Container = styled.div<ContainerProps>`
@@ -13,10 +16,16 @@ const Container = styled.div<ContainerProps>`
   height: 200px;
   background-color: ${(props) => props.bgColor};
   border-radius: 100px;
+  border: 1px solid ${(props) => props.borderColor};
 `;
 
-function Circle({ bgColor }: CircleProps) { // (2) Component에 전달한다. typescript를 통해 입력된 props 자동입력 및 존재하지 않는 props는 에러처리
-  return <Container bgColor={bgColor} />; // <Container>가 <div>이므로 또 interface가 요구됨. 그래서 7번 째 줄에 해당 interface 추가.
+function Circle({ bgColor, borderColor, text="default text" }: CircleProps) { // (2) Component에 전달한다. typescript를 통해 입력된 props 자동입력 및 존재하지 않는 props는 에러처리
+  return <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}>
+      {text}
+    </Container>; 
+/*   <Container>가 <div>이므로 또 interface가 요구됨. 그래서 상단에 해당 ContainerProps interface 추가. interface를 추가하기 전에는 <Container>의 props(보라색 bgColor)에 오류 발생.
+borderColor의 경우, optional prop이기 때문에 상단의 CircleProps interface에서는 ?로 optional하지만, CotainerProps interface에서는 required로 정의되어 있어 오류 발생. 이를 해결하기 위해 ??로 default(optianal일 때 입력값이 없으면 default를 적용)를 입력하여 에러 해결  */
+  
 }
 /* function Circle(props: CircleProps) { 
   return <Container bgColor={props.bgColor} />;
