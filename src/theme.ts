265394ce44262974c51b5b4e7cf9573cd13ce999
sadpu111 <dwme7100@gmail.com>
2022-10-04 22:22:24 +0900

    theme

---
+++ src/theme.ts
@@ -0,0 +1,15 @@
+import {DefaultTheme} from "styled-components"
+
+const lightTheme:DefaultTheme = {
+  bgColor: "white",
+  textColor: "black",
+  btnColor: "teal",
+}
+
+const darkTheme:DefaultTheme = {
+  bgColor: "black",
+  textColor: "white",
+  btnColor: "tomato",
+}
+
+export {lightTheme, darkTheme}
